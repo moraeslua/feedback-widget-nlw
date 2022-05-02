b38f661e46a89825bd28e7ce29241ff5aff19164
moraes <moraeslua66@gmail.com>
2022-05-02 10:49:05 -0300

    feat: implement popover functionality

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -3,23 +3,15 @@ import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from "@headlessui/react";
 
 export default function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-  function toggleWidthVisibility() {
-    setIsWidgetOpen(!isWidgetOpen);
-  }
-
   return (
-    <div className="absolute bottom-5 right-5">
-      <button
-        onClick={toggleWidthVisibility}
-        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
-      >
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hello World</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
