1eb42c5dcd3dba51e481b1fa096c7af1ff6f3161
moraes <moraeslua66@gmail.com>
2022-05-02 10:10:02 -0300

    feat: add component Widget with initial style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Widget from "./components/Widget";
+import { ChatTeardropDots } from "phosphor-react";
+
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <button className="bg-brand-500 rounded-full px-3 h-12 text-white">
+      <ChatTeardropDots className="w-6 h-6" />
+    </button>
+  );
 }
 
 export default App;
---
+++ src/components/Widget.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Widget() {
+  return <h2>Widget</h2>;
+}
