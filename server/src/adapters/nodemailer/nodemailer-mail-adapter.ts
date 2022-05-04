4e5ab6c9bdf3e88a6294c63980bee42790366919
moraes <moraeslua66@gmail.com>
2022-05-04 15:56:56 -0300

    feat: apply dependency inversion principle on server

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7185685910f76b",
+    pass: "81cc8a76abf686",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ body, subject }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Luana de Moraes <moraeslua66@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,45 +1,10 @@
 import express from "express";
-import { prisma } from "./prisma";
-import nodemailer from "nodemailer";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "7185685910f76b",
-    pass: "81cc8a76abf686",
-  },
-});
-
-app.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
-
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedget.com>",
-    to: "Luana de Moraes <moraeslua66@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div styles="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feeedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`,
-    ].join(""),
-  });
-
-  return res.status(201).json({ data: feedback });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("HTTP server running on 3333 port");
