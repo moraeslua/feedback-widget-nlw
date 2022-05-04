24c23ea861b00bfc81d332e917101410fb0eec87
moraes <moraeslua66@gmail.com>
2022-05-04 12:20:34 -0300

    feat: config mailtrap to receive feedbacks on email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7185685910f76b",
+    pass: "81cc8a76abf686",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Luana de Moraes <moraeslua66@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div styles="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feeedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join(""),
   });
 
   return res.status(201).json({ data: feedback });
