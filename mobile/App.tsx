4e5c67c806cd55d34e502263fe7e90e659c7fb3e
moraes <moraeslua66@gmail.com>
2022-05-06 19:41:14 -0300

    feat: add custom fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,24 @@
 import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 import { View } from "react-native";
 import { theme } from "./src/theme";
 import { Widget } from "./src/components/Widget";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View
       style={{
