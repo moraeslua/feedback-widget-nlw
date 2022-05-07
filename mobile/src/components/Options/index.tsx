2fabcdac9529fe391eb051733064b11bb0509e06
moraes <moraeslua66@gmail.com>
2022-05-07 13:52:48 -0300

    feat: add feedback options in widget

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { feedbackTypes } from "../../utils/feedbackTypes";
 
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { Copyright } from "../Copyright";
 
 import { styles } from "./styles";
+import { Option } from "../Option";
+
+const feedbackTypesEntries = Object.entries(feedbackTypes);
 
 export function Options() {
   return (
     <View style={styles.container}>
+      <Text>Deixe seu feedback</Text>
+      <View style={styles.options}>
+        {feedbackTypesEntries.map(([key, value]) => (
+          <Option key={key} title={value.title} image={value.image} />
+        ))}
+      </View>
       <Copyright />
     </View>
   );
