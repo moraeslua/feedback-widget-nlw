9d160c04e119e8505b48daede3832ff146acd73a
moraes <moraeslua66@gmail.com>
2022-05-08 11:47:03 -0300

    feat: create sucess component

---
+++ mobile/src/components/Sucess/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import sucessImageUrl from "../../assets/sucess.png";
+
+import { styles } from "./styles";
+
+export function Sucess() {
+  return <View style={styles.container}></View>;
+}
