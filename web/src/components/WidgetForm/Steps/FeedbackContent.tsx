3207bb09421706746058ecb671df39aeed8d513b
moraes <moraeslua66@gmail.com>
2022-05-08 20:27:39 -0300

    feat: add loading status in send feedback button

--- web/src/components/WidgetForm/Steps/FeedbackContent.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContent.tsx
@@ -5,6 +5,7 @@ import { ArrowLeft } from "phosphor-react";
 import ScreenshotButton from "../ScreenshotButton";
 import { FormEvent, useState } from "react";
 import { api } from "../../../lib/api";
+import Loading from "../Loading";
 
 const minCharactersNumberForComment = 3;
 
@@ -15,10 +16,12 @@ export default function FeedbackContent({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
   const { title, image } = feedbackTypes[feedbackType];
 
   const handleSubmitFeedback = async (event: FormEvent) => {
     event.preventDefault();
+    setIsSendingFeedback(true);
     try {
       await api.post("/feedbacks", {
         type: feedbackType,
@@ -29,6 +32,7 @@ export default function FeedbackContent({
     } catch (error) {
       console.log(error);
     }
+    setIsSendingFeedback(false);
   };
 
   return (
@@ -63,7 +67,7 @@ export default function FeedbackContent({
             disabled={comment.length < minCharactersNumberForComment}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : "Enviar feedback"}
           </button>
         </footer>
       </form>
