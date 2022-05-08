fd2bf3e1d3f4e63261faa187d4c1ab6425732d0b
moraes <moraeslua66@gmail.com>
2022-05-08 20:31:09 -0300

    feat: disable send feedback button while feedback is sending

--- web/src/components/WidgetForm/Steps/FeedbackContent.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContent.tsx
@@ -64,7 +64,10 @@ export default function FeedbackContent({
           />
           <button
             type="submit"
-            disabled={comment.length < minCharactersNumberForComment}
+            disabled={
+              comment.length < minCharactersNumberForComment ||
+              isSendingFeedback
+            }
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
             {isSendingFeedback ? <Loading /> : "Enviar feedback"}
